f0237d4a7375e75d50c8ae7da7e671f1ffc94cb3
bacebu4 <vasua14735@icloud.com>
2023-07-16 09:41:26 +0300

    feat: basic auth flow

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,22 @@
 import { ExtensionContext } from 'vscode';
+import { registerCredentials } from './authentication';
 import { registerIssueView } from './issue/registerIssueView';
+import { SentryApi } from './sentry-api';
 
-export function activate(context: ExtensionContext) {
-  registerIssueView(context);
+export async function activate(context: ExtensionContext) {
+  const sentryApi = new SentryApi();
+
+  const { login } = await registerCredentials(context);
+
+  const credentials = await login();
+
+  if (!credentials) {
+    throw new Error();
+  }
+
+  sentryApi.setOptions({ host: credentials.instanceUrl, token: credentials.token });
+
+  await registerIssueView(context, sentryApi);
 }
 
 export function deactivate() {}
