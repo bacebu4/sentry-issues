e84067bd91f2dde9d0bdba2b6f62f9780d8f1679
bacebu4 <vasua14735@icloud.com>
2023-07-16 10:17:47 +0300

    feat(shared): never guard

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -2,3 +2,4 @@ export { List } from './List';
 export { ListDataProvider } from './ListDataProvider';
 export { VS_COMMANDS } from './vsCommands';
 export { Result } from './Result';
+export { exhaustiveMatchingGuard } from './exhaustiveMatchingGuard';
