2e44ba17f8b21af1440cbb22992b95228f6c8e60
bacebu4 <vasua14735@icloud.com>
2023-07-18 08:37:06 +0300

    refactor: better name for issue view

--- src/issue/constants.ts
+++ src/issue/constants.ts
@@ -1,6 +1,6 @@
 export const ISSUE_CONTEXT_VALUE = 'issueItem';
 
-export const ISSUE_VIEW_ID = 'testView';
+export const ISSUE_VIEW_ID = 'issueView';
 
 export const ISSUE_COMMANDS = {
   refreshIssues: `${ISSUE_VIEW_ID}.refreshEntry`,
