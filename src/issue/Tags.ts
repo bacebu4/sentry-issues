2f70dc43bd5405bd58cef10567574f910748c2b6
bacebu4 <vasua14735@icloud.com>
2023-07-27 22:15:42 +0300

    refactor(issue): split tags counting within class

--- src/issue/Tags.ts
+++ src/issue/Tags.ts
@@ -1,23 +1,26 @@
 export class Tags {
-  public constructor(private readonly rawTags: { key: string; value: string }[]) {}
+  private tagsMap: Map<string, Map<string, number>>;
+
+  public constructor(private readonly rawTags: { key: string; value: string }[]) {
+    this.tagsMap = new Map(this.rawTags.map(t => [t.key, new Map()]));
+
+    this.rawTags.forEach(t => {
+      const correspondingMap = this.tagsMap.get(t.key);
+      const valueOfCorrespondingMap = correspondingMap?.get(t.value);
+      if (valueOfCorrespondingMap !== undefined) {
+        correspondingMap?.set(t.value, valueOfCorrespondingMap + 1);
+      } else {
+        correspondingMap?.set(t.value, 1);
+      }
+    });
+  }
 
   public get values(): {
     key: string;
     values: { value: string; count: number; percentage: string }[];
   }[] {
-    const result = new Map(this.rawTags.map(t => [t.key, new Map()]));
-
-    this.rawTags.forEach(t => {
-      const correspondingMap = result.get(t.key);
-      if (correspondingMap?.has(t.value)) {
-        correspondingMap.set(t.value, correspondingMap.get(t.value) + 1);
-      } else {
-        correspondingMap?.set(t.value, 1);
-      }
-    });
-
-    return [...result.entries()].map(([key, values]) => {
-      const overallCountPerKey = [...values.entries()].reduce((acc, val) => acc + val[1], 0);
+    return [...this.tagsMap.entries()].map(([key, values]) => {
+      const overallCountPerKey = [...values.entries()].reduce((acc, [, count]) => acc + count, 0);
 
       return {
         key,
