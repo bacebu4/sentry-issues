0ce8eeb6fccf689a736e2e228bf293ed10dbf5b0
bacebu4 <vasua14735@icloud.com>
2023-07-27 22:03:02 +0300

    feat(shared): bullet content

---
+++ src/shared/BulletContent.ts
@@ -0,0 +1,15 @@
+export class BulletContent {
+  public constructor(private readonly title: string, private readonly values: [string, string][]) {}
+
+  private get longestKeyLength(): number {
+    return this.values.map(([key]) => key.length).reduce((acc, val) => Math.max(acc, val));
+  }
+
+  public toString(): string {
+    const bullets = this.values
+      .map(([key, value]) => `- ${key.padEnd(this.longestKeyLength + 1)}: ${value}`)
+      .join('\n');
+
+    return [this.title, bullets + '\n'].join('\n\n');
+  }
+}
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -2,3 +2,4 @@ export { List } from './List';
 export { ListDataProvider } from './ListDataProvider';
 export { VS_COMMANDS } from './vsCommands';
 export { createTreeView } from './createTreeView';
+export { BulletContent } from './BulletContent';
